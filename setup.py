5ec0bf801b027978be55e48d33f5452a89b60d49
Raphaël <r.courivaud@gmail.com>
2017-07-22 15:48:31 +0200

    change to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-from distutils.core import setup
-
 from pip.req import parse_requirements
+from setuptools import setup
 
 install_reqs = parse_requirements("requirements.txt")
 reqs = [str(ir.req) for ir in install_reqs]
